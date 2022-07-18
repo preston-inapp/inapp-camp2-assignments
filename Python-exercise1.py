d688a0e0ad93dc8b67cd446ecc06e76b2812426b
preston <preston.j@inapp.com>
2022-07-18 12:39:52 +0530

    added python first assignment

---
+++ Python-exercise1.py
@@ -0,0 +1,5 @@
+text = "{} is my country. All Indians are my brothers and sisters.".format('India')
+print(text)
+
+count = text.count('India')
+print("Count of India in the text is {}".format(count))
