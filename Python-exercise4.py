4fc640c0ac004a0f7f79eb10be5d1feb916cfbd7
preston <preston.j@inapp.com>
2022-07-19 14:58:34 +0530

    added fourth assignment

---
+++ Python-exercise4.py
@@ -0,0 +1,12 @@
+#Print the list of numbers which are divisible by 5 and multiple of 8 between 2000 and 2500(both included)
+nums = []
+for i in range(2000, 2501):
+    if (i%5 == 0 and i%8 == 0):
+        nums.append(i)
+print(nums)
+
+#Write a Python program to create the multiplication table (from 1 to 10) of a number getting input from the user
+_num = int(input())
+
+for i in range(1, 11):
+    print(_num, "*", i, "=", _num*i)
